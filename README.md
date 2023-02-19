66240c859206650ad58c440867d76139e7a681a7
Lerac1275 <carelchay@outlook.com>
2023-02-19 22:11:47 +0800

    added model training and evaluation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Introduction
 
 This project contains the notebooks and files used for a data science project task.
+
+# notebooks
+This is the main folder for this project, containing the notebook files used in :
+1. Preprocessing the dataset (`1. preprocessing.ipynb`)
+2. Doing some exploratory data analysis on the dataset (`2. analysis.ipynb`)
+3. Conducting the __model training and evaluation__ for this machine learning task. (`3. Model Training.ipynb`)<br>
+    This is the notebook with the infromation most relevant to this data science project. 
+
+This folder also contains `helper.py`, which houses some custom utility function used in plotting and tabulation within `3. Model Training.ipynb`. 
+
+# datasets
+This folder contains the raw data used for this task (`ICU_raw.csv`) along with the preprocessed version saved to an sqlite db (`ICU_data.sqlite`)
--- notebooks/helper.py
+++ notebooks/helper.py
@@ -5,6 +5,40 @@ from sklearn.metrics import PrecisionRecallDisplay, precision_recall_curve
 from sklearn.metrics import RocCurveDisplay
 from sklearn.preprocessing import LabelBinarizer
 from sklearn.metrics import precision_score, recall_score
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import re
+
+def tablulate_confusion_matrix(classification_report_collection:dict, exclude_models = [], drop_metrics =['macro avg', 'weighted avg'])->pd.DataFrame:
+    """
+    This is a helper function to tabulate a given set of confusion matrices into a single pandas dataframe. 
+
+    Assumes the input is a dictionary where the keys are the model name, the values are the dictionary returned by classification_report
+
+    Parameters
+    ----------
+    exclude_models : list[str]
+        Which models to exclude from the tabulation. Does a regular expression match so no need to input the entire model name
+    drop_metrics : list[str]
+        Which metrics to exclude. By default excludes the macro avg and weighted avg
+    """
+    r = pd.DataFrame()
+    for k,v in classification_report_collection.items():
+        if any (list(map(lambda x : re.search(x, k) is not None, exclude_models))):
+            continue
+        tmp = pd.DataFrame(v)
+        # Drop desired metrics
+        tmp = tmp.drop(drop_metrics, axis=1).T
+        # Overwrite incorrect accuracy / support values
+        tmp.iloc[2, 0:2] = np.nan
+        tmp.iloc[2, 3] = tmp.iloc[0:2, 3].sum()
+        tmp = pd.concat({k: tmp}, names=['Model'])
+        r = pd.concat([r, tmp])
+    # Round of values
+    r[r.columns.difference(['suppport'])] = r[r.columns.difference(['suppport'])].round(2)
+    r['support'] = r['support'].round(0).astype(int)
+    return r
 
 def plot_ROC_PR(y_test, y_probas, model_name, label_to_plot, figsize = (13, 3.5)):
     """
@@ -31,7 +65,15 @@ def plot_ROC_PR(y_test, y_probas, model_name, label_to_plot, figsize = (13, 3.5)
     ax[0].set_title(f"ROC for {label_to_plot}")
     # Fit the PR curve
     PrecisionRecallDisplay.from_predictions(y_test, y_probas, name = model_name, ax = ax[1])
-    ax[1].plot([0, 1], [0, 0], "k--", label="No Skil")
+    ax[1].plot([0, 1], [0, 0], "k--", label="No Skill")
+    # Compute PR statistics
+    precision, recall, thresholds = precision_recall_curve(y_test, y_probas)
+    # Compute f1-score
+    fscore = (2 * precision * recall) / (precision + recall)
+    # locate the index of the largest f score
+    ix = np.argmax(fscore)
+    ax[1].scatter(recall[ix], precision[ix], marker='o', color='black',\
+                   label=f'Best (Threshold={thresholds[ix]:.2f}, f-score={fscore[ix]:.2f})')
     ax[1].legend()
     ax[1].set_title(f"PR Curve for {label_to_plot}")
     
@@ -50,10 +92,40 @@ def plot_precision_recall_vs_threshold(y_test, y_probas, figsize = (13, 3.5)):
     None
     """
     precisions, recall, thresholds = precision_recall_curve(y_test, y_probas)
-    fig = plt.figure(figsize= figsize)
-    plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
-    plt.title("Precision & Recall by threshold value")
+    # Compute f1-score
+    fscore = (2 * precisions * recall) / (precisions + recall)
+    # locate all the indexes of the largest f score. There may be multiple combinations of threshold-precision-recall that produce the greatest fscore
+    ixs = np.argwhere(fscore==np.max(fscore))
+    # Best fscore and corresponding threshold value
+    best_combi = list(map(lambda ix : (thresholds[ix][0], fscore[ix][0], precisions[ix][0], recall[ix][0]) , ixs))
+    ## Plotting ##
+    fig, ax = plt.subplots(figsize= figsize)
+    # Plot the precision
+    plt.plot(thresholds, precisions[:-1], "b-", label="Precision")
+    plt.title("Precision, Recall & fscore by threshold value")
     plt.xlabel("Threshold value")
     plt.ylabel("Score")
+    # Plot the Recall
     plt.plot(thresholds, recall[:-1], "g-", label="Recall")
-    plt.legend()
+    # Plot the fscore
+    plt.plot(thresholds, fscore[:-1], "r-", label="fScore")
+    # Plot out all the best fscores
+    for best_threshold, best_fscore, best_precision, best_recall in best_combi:
+        plt.plot([0, best_threshold], [best_fscore, best_fscore], "k--")
+        plt.plot([best_threshold, best_threshold], [0, best_fscore], "k--")
+        plt.scatter(best_threshold, best_fscore, marker='o', color='black',\
+                    label=f"fscore={best_fscore:.2f} threshold={best_threshold:.2f}\nPrecision={best_precision:.2f} Recall={best_recall:.2f}")
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.2),
+          fancybox=True, shadow=True, ncol=5)
+    return best_combi
+
+# Helper function to define color palettes
+def colors_from_values(values, palette_name):
+    values = values.astype(int)
+    # normalize the values to range [0, 1]
+    normalized = (values - min(values)) / (max(values) - min(values))
+    # convert to indices
+    indices = np.round(normalized * (len(values) - 1)).astype(np.int32)
+    # use the indices to get the colors
+    palette = sns.color_palette(palette_name, len(values))
+    return np.array(palette).take(indices, axis=0)
